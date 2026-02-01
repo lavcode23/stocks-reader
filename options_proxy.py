b76fe21cc3947d050c2b698ad51d0d266a6d5ed2
Lavisha Yadav <117895987+lavcode23@users.noreply.github.com>
2026-02-01 21:31:57 +0530

    Update options_proxy.py

--- options_proxy.py
+++ options_proxy.py
@@ -1,6 +1,6 @@
-def apply_protective_put_proxy(weekly_return: float, premium_bps: float, crash_protect_pct: float) -> float:
-    premium = premium_bps / 10000.0
-    r = weekly_return - premium
-    if r < -crash_protect_pct:
-        r = -crash_protect_pct - premium
+def protective_put_proxy(weekly_ret, premium_bps=20, crash_cap=0.03):
+    premium = premium_bps/10000.0
+    r = weekly_ret - premium
+    if r < -crash_cap:
+        r = -crash_cap - premium
     return r
